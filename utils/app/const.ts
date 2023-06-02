3228e8091257b2dc41c25959bf26c3856bc97b81
Anthony DePasquale <anthony@depasquale.org>
2023-06-02 11:02:57 +0200

    Customize

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,12 +1,12 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "You are a helpful assistant. Follow the user's instructions carefully. Respond using markdown. Don't include unnecessary text such as an equivocating preamble or a restatement of the question. Just respond concisely, accurately, and expertly.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
 export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.5");
 
 export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
